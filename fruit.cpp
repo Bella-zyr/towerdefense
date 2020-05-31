b2fe0c96a795ef1cc49e4fbfcc6d59743a01be7f
Bella-zyr <1279918064@qq.com>
2020-05-31 13:20:40 +0800

    第一个版本，已经实现的是：取出了石头和稻草人，添加了两张背景图片，还增加了一个按钮，控制两张图片互相切换，增加了背景音乐，稻草人的移动通过 WASD来控制，就是图片并不能全屏显示，切换时图片比较小。

--- fruit.cpp
+++ fruit.cpp
@@ -10,10 +10,10 @@ Fruit::~Fruit()
 
 }
 
-/*void Fruit::onErase(){
+void Fruit::onErase(){
     QMediaPlayer * player = new QMediaPlayer;
     player->setMedia(QUrl("qrc:/sounds/crash.mp3"));//ÉèÖÃÉùÒô
     player->setVolume(30);
     player->play();
-}*/
+}
 
